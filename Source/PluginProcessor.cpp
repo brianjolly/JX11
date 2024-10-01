e0835c016cbf8bd9e0da048507ff8248fba4b5c3
Brian Jolly <brian@brianjolly.com>
2024-10-01 16:01:31 -0700

    Basic synth midi handling

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -93,14 +93,13 @@ void JX11AudioProcessor::changeProgramName (int index, const juce::String& newNa
 //==============================================================================
 void JX11AudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    synth.allocateResources(sampleRate, samplesPerBlock);
+    reset();
 }
 
 void JX11AudioProcessor::releaseResources()
 {
-    // When playback stops, you can use this as an opportunity to free up any
-    // spare memory, etc.
+    synth.deallocateResources();
 }
 
 #ifndef JucePlugin_PreferredChannelConfigurations
@@ -186,14 +185,17 @@ void JX11AudioProcessor::splitBufferByEvents(juce::AudioBuffer<float>& buffer,
 
 void JX11AudioProcessor::handleMIDI(uint8_t data0, uint8_t data1, uint8_t data2)
 {
-    char s[16];
-    snprintf(s, 16, "%02hhX %02hhX %02hhX", data0, data1, data2);
-    DBG(s);
+    synth.midiMessage(data0, data1, data2);
 }
 
 void JX11AudioProcessor::render(juce::AudioBuffer<float> & buffer, int sampleCount, int bufferOffset)
 {
     // do nothing yet
+}
+
+void JX11AudioProcessor::reset()
+{
+    synth.reset();
 }
 
 //==============================================================================
